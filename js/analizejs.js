1856ea5d93c12adbdb6f0d68831ba2aceebac842
Madi <maditop.12345@gmail.com>
2022-09-22 00:28:00 +0600

    создал 2 графика для анализа

--- js/analizejs.js
+++ js/analizejs.js
@@ -11,29 +11,54 @@ $(function(){
             }),
             success: (function(data) {
                 var arr = data.split('|');
-                document.getElementById('1').innerHTML = arr[0];
-                document.getElementById('2').innerHTML = arr[1];
-                document.getElementById('3').innerHTML = arr[2];
-                document.getElementById('4').innerHTML = arr[3];
-                document.getElementById('5').innerHTML = arr[4];
-                document.getElementById('6').innerHTML = arr[5];
-                document.getElementById('7').innerHTML = arr[6];
-                document.getElementById('8').innerHTML = arr[7];
-                document.getElementById('bakalavr1').innerHTML = arr[8];
-                document.getElementById('bakalavr2').innerHTML = arr[9];
-                document.getElementById('bakalavr3').innerHTML = arr[10];
-                document.getElementById('bakalavr4').innerHTML = arr[11];
-                document.getElementById('bakalavr5').innerHTML = arr[12];
-                document.getElementById('bakalavr6').innerHTML = arr[13];
-                document.getElementById('bakalavr7').innerHTML = arr[14];
-                document.getElementById('bakalavr8').innerHTML = arr[15];
-                document.getElementById('bakalavr9').innerHTML = arr[16];
-                document.getElementById('bakalavr10').innerHTML = arr[17];
-                document.getElementById('bakalavr11').innerHTML = arr[18];
-                document.getElementById('bakalavr12').innerHTML = arr[19];
-                document.getElementById('magphd13').innerHTML = arr[20];
-                document.getElementById('magphd14').innerHTML = arr[21];
-                document.getElementById('colluge15').innerHTML = arr[22];
+                new Chart(document.getElementById("bar-chart1"), {
+                    type: 'line',
+                    data: {
+                        labels: ["9:00-10:00", "10:00-11:00", "11:00-12:00", "12:00-13:00", "14:00-15:00", "15:00-16:00", "16:00-17:00", "17:00-18:00",],
+                        datasets: [
+                            {
+                                label: "Кол.во людей",
+                                data: [arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7]],
+                                borderColor: 'orange',
+                                backgroundColor: 'white',
+                                pointBorderColor: 'orange',
+                                pointBackgroundColor: 'rgba(255,150,0,0.5)'
+
+                            }
+                        ]
+                    },
+                    options: {
+                        legend: { display: false },
+                        title: {
+                            display: true,
+                            text: 'Кол.во людей в отрезке часов'
+                        }
+                    }
+                });
+                new Chart(document.getElementById("bar-chart2"), {
+                    type: 'line',
+                    data: {
+                        labels: ["bakalavr1", "bakalavr2", "bakalavr3", "bakalavr4", "bakalavr5", "bakalavr6", "bakalavr7","bakalavr8", "bakalavr9", "bakalavr10", "bakalavr11", "bakalavr12", "magphd13", "magphd14", "colluge15"],
+                        datasets: [
+                            {
+                                label: "Кол.во людей",
+                                data: [arr[8],arr[9],arr[9],arr[10],arr[11],arr[12],arr[13],arr[14],arr[15],arr[16],arr[17],arr[18],arr[19],arr[20],arr[21],arr[22]],
+                                borderColor: 'orange',
+                                backgroundColor: 'white',
+                                pointBorderColor: 'orange',
+                                pointBackgroundColor: 'rgba(255,150,0,0.5)'
+
+                            }
+                        ]
+                    },
+                    options: {
+                        legend: { display: false },
+                        title: {
+                            display: true,
+                            text: 'Кол.во людей в отрезке часов'
+                        }
+                    }
+                });
                 $("#updatebutton").prop("disabled", false);
             })
         })
